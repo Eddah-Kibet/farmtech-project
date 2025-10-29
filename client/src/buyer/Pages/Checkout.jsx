dbcaa52bd3a2225bd846486250da74a21a3b1989
Danbradleystakusopere <danstakus@gmail.com>
2025-10-29 23:07:41 +0300

    added Checkout page with checkout form integration

---
+++ client/src/buyer/Pages/Checkout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CheckoutForm from "../components/CheckoutForm";
+
+function Checkout() {
+  return (
+    <div className="checkout-page">
+      <h2>Checkout</h2>
+      <CheckoutForm />
+    </div>
+  );
+}
+
+export default Checkout;
