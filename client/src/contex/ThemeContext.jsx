10bfc1b689e363984ab0c75f49742a991833494d
sharon <njerisharon84@gmail.com>
2025-10-30 14:20:52 +0300

    add body class management

--- client/src/contex/ThemeContext.jsx
+++ client/src/contex/ThemeContext.jsx
@@ -58,3 +58,20 @@ return (
     {children}
   </ThemeContext.Provider>
 );
+const toggleTheme = () => {
+  const newTheme = !isDarkMode;
+  setIsDarkMode(newTheme);
+  localStorage.setItem('theme', newTheme ? 'dark' : 'light');
+
+  if (newTheme) {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
+};
+
+return (
+  <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+    {children}
+  </ThemeContext.Provider>
+);
