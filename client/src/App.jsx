7481ed4b6c151f85b06a5d83204e2a5d8a0af6ea
Eddah kibet <eddahkibet20@gmail.com>
2025-10-30 13:27:40 +0300

    made necessary files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,9 @@
 import { Routes, Route } from "react-router-dom"; 
-import HomePage from "./pages/HomePage";
-import SideBar from "./components/SideBar";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/sidebar" element={<SideBar />} />
+
     </Routes>
   );
 }
