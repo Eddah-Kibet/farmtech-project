bb17693a914796ab90a778dced324e8867917386
sharon <njerisharon84@gmail.com>
2025-10-30 13:52:31 +0300

    initialize cart context and provider stracture

---
+++ client/src/contex/CartContext.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const CartContext = createContext();
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+  return context;
+};
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  return (
+    <CartContext.Provider value={{ cart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
